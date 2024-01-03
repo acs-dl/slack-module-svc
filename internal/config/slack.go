d6c343957fda1f289e38335c3f7f9897bf2cfd13
Danylo Ostapchenko <danya.ostap@gmail.com>
2024-01-03 17:35:03 +0200

    Add validation to Slack config

--- internal/config/slack.go
+++ internal/config/slack.go
@@ -2,6 +2,15 @@ package config
 
 import (
 	"os"
+	"regexp"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/pkg/errors"
+)
+
+const (
+	userTokenRegex = "xoxp-[0-9]{13}-[0-9]{13}-[0-9]{13}-[0-9a-zA-Z]{32}"
+	botTokenRegex  = "xoxb-[0-9]{13}-[0-9]{13}-[0-9a-zA-Z]{24}"
 )
 
 type SlackConfig struct {
@@ -11,19 +20,22 @@ type SlackConfig struct {
 
 func (c *config) SlackParams() SlackConfig {
 	return c.slackCfg.Do(func() interface{} {
-		userToken := os.Getenv("USER_TOKEN")
-		if userToken == "" {
-			panic("no user token was provided")
+		config := SlackConfig{
+			UserToken: os.Getenv("USER_TOKEN"),
+			BotToken:  os.Getenv("BOT_TOKEN"),
 		}
 
-		botToken := os.Getenv("BOT_TOKEN")
-		if botToken == "" {
-			panic("no bot token was provided")
+		if err := config.validate(); err != nil {
+			panic(errors.Wrap(err, "slack oauth tokens validation failed"))
 		}
 
-		return SlackConfig{
-			UserToken: userToken,
-			BotToken:  botToken,
-		}
+		return config
 	}).(SlackConfig)
+}
+
+func (cfg *SlackConfig) validate() error {
+	return validation.Errors{
+		"USER_TOKEN": validation.Validate(&cfg.UserToken, validation.Required, validation.Match(regexp.MustCompile(userTokenRegex))),
+		"BOT_TOKEN":  validation.Validate(&cfg.BotToken, validation.Required, validation.Match(regexp.MustCompile(botTokenRegex))),
+	}.Filter()
 }
